9c53f8253a89446b34fa846863f033f63d3059f1
desertcoqui <fdetres@my365.bellevue.edu>
2022-01-12 14:28:10 -0600

    index file written from template

--- index.mjs
+++ index.mjs
@@ -1,8 +1,8 @@
-import {readFile} from 'fs/promises'
+import {readFile, writeFile} from 'fs/promises'
 import {nebraska} from "./second.mjs"
-//index.html file
+//template.html file
 //utf-8 allows us not to have to use the toString method when calling the html file
-let template = await readFile(new URL('index.html', import.meta.url), 'utf-8')
+let template = await readFile(new URL('template.html', import.meta.url), 'utf-8')
 
 const data = {
     title: 'Planning the move',
@@ -12,6 +12,9 @@ const data = {
  for (const [k,v] of Object.entries(data)){
 template = template.replace(`{${k}}`, v)
  } 
+
+ //writing template.html to index.html
+ await writeFile (new URL ('index.html', import.meta.url), template)
 
 console.log(template)
 
