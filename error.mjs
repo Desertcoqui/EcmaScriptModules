01d6ef5860d36a3dd89c7fed4e766854f8862da7
desertcoqui <fdetres@my365.bellevue.edu>
2022-01-12 15:04:26 -0600

    error handling with fs/promises

---
+++ error.mjs
@@ -0,0 +1,10 @@
+import {readFile} from 'fs/promises'
+
+
+//catches error -lets code continue without breaking the app
+
+
+   const result = readFile(new URL ('app.mjs', import.meta.url), 'utf-8')  
+.catch (e =>{
+    console.log("Yes we know there is can error, but carry on")
+})
