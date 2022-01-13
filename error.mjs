51ccc85fb8e9dc9f7e50ab5832fdf1fc5d150d3f
desertcoqui <fdetres@my365.bellevue.edu>
2022-01-12 19:48:40 -0600

    error catch

--- error.mjs
+++ error.mjs
@@ -3,8 +3,7 @@ import {readFile} from 'fs/promises'
 
 //catches error -lets code continue without breaking the app
 
-
    const result = readFile(new URL ('app.mjs', import.meta.url), 'utf-8')  
 .catch (e =>{
     console.log("Yes we know there is can error, but carry on")
-})
+}) 
